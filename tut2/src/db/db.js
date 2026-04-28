e498790dd0798bb35facb17f89e692251a9eb09c
tanyaagraharicoder <146922171+tanyaagraharicoder@users.noreply.github.com>
2026-04-29 00:29:49 +0530

    Update db.js

--- tut2/src/db/db.js
+++ tut2/src/db/db.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 async function connectDB() {
   try {
     await mongoose.connect(
-      "mongodb://yt:<Password>X@ac-f7xof4i-shard-00-00.d5z3jne.mongodb.net:27017,ac-f7xof4i-shard-00-01.d5z3jne.mongodb.net:27017,ac-f7xof4i-shard-00-02.d5z3jne.mongodb.net:27017/tanya?ssl=true&replicaSet=atlas-p8acpu-shard-0&authSource=admin&retryWrites=true&w=majority"
+      "Database String  from mongodb"
     );
 
     console.log("✅ Connected to DB");
