76afbbaba5c875eb154f5b62acf54186b69dacd1
tanyaagraharicoder <rg96958227@gmail.com>
2026-04-30 22:55:03 +0530

    connect backend with  fronted using axios and handled CORS policy successfully

--- Frontend/src/pages/CreatePost.jsx
+++ Frontend/src/pages/CreatePost.jsx
@@ -1,10 +1,33 @@
 import React from 'react'
+import axios from 'axios'
+import{useNavigate} from 'react-router-dom'
+
 
 const CreatePost = () => {
+    const navigate = useNavigate();
+    const handleSubmit = async (e) => {
+  e.preventDefault();
+
+  console.log("Submitting...");
+
+  const formData = new FormData(e.target);
+
+  try {
+    const res = await axios.post("http://localhost:3000/create-post", formData);
+    console.log("Success:", res);
+
+    navigate('/feed');
+
+  } catch (err) {
+    console.error("API Error:", err);
+  }
+};
+
+
   return (
     <section className='create-post-section'>
       
-      <form className="post-form">
+      <form onSubmit={handleSubmit} className="post-form">
         <h1 className="form-title">Create Post</h1>
 
         <label className="file-upload">
--- Frontend/src/pages/Feed.jsx
+++ Frontend/src/pages/Feed.jsx
@@ -1,14 +1,29 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+
+import axios from 'axios'
 
 const Feed = () => {
 
-  const [posts] = useState([
+  const [posts, setPosts] = useState([
     {
       _id: 1,
       image: "https://images.unsplash.com/photo-1628827720737-d8ad84806c35?q=80&w=464&auto=format&fit=crop",
       caption: "Beautiful sunset at the beach!"
     }
   ])
+
+//    here we connect backend using axious and fetch the posts from database and set it to state and then we map through the posts and display them in feed section
+
+  useEffect(() => {
+
+axios.get("http://localhost:3000/posts")
+.then(res => {
+    setPosts(res.data.posts)
+  
+})
+
+  }, [])
+
 
   return (
     <section className='feed-section'>
